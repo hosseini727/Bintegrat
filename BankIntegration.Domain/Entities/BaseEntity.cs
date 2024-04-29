3ff752ca6ec9b2228c69d11afb49e33f6bd1333c
Mohammadali Ghazi <m-ghazi@sos.com>
2024-04-29 12:29:42 +0330

    remove cash from develop

--- BankIntegration.Domain/Entities/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankIntegration.Domain.Entities;
-
-public class BaseEntity
-{
-    public long Id { get; set; }
-    public DateTime?  CreateDate { get; set; }
-    public DateTime? ModifyDate { get; set; }
-}
