cdcd5b64ee3a7c080894bb370e0f9ac599f30cdf
AliGhazi <mr.alighazi89@gmail.com>
2024-09-01 09:02:05 +0330

    modify notification of sheba inquiry

--- BankIntegration.Service/Services/InquiryBankService.cs
+++ BankIntegration.Service/Services/InquiryBankService.cs
@@ -20,9 +20,9 @@ public class InquiryBankService : IInquiryBankService
         // handleQuery
         var query = new GetInquiryShebaQuery(accountNo);
         var result = await _mediator.Send(query);
-        // handleEvent Develop
+        // handleEvent
         var notification = new GetShebaInquiryNotificationResponse(result);
-        await _mediator.Publish(notification, default);
+        await _mediator.Publish(notification);
         return result;
     }
     public async Task<ConvertAccountNoResponseModel> ConvertAccountNo(string depositNo)
