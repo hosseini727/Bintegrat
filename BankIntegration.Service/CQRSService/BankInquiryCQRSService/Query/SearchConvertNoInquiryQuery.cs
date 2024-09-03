41a8e54397982def95119c4e8a7b0ddb0703fc39
e-hosseini <esmaeil.hosseini727@gmail.com>
2024-09-03 11:29:59 +0330

    add search convertNo

---
+++ BankIntegration.Service/CQRSService/BankInquiryCQRSService/Query/SearchConvertNoInquiryQuery.cs
@@ -0,0 +1,20 @@
+﻿using BankIntegration.Service.Model.BankInquiry;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankIntegration.Service.CQRSService.BankInquiryCQRSService.Query
+{
+    public class SearchConvertNoInquiryQuery: IRequest<IEnumerable<ConvertAccountNoResponseModel>>
+    {
+        public string SearchText { get; }
+        public SearchConvertNoInquiryQuery(string searchText)
+        {
+            SearchText = searchText;
+        }
+    }
+
+}
