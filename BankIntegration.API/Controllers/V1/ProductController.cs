c6cbcc1fd667d3286542513afb9e82f691ddcd9f
e-hosseini <esmaeil.hosseini727@gmail.com>
2024-09-07 14:23:11 +0330

    Revert "modify fixture in test"
    This reverts commit 291d579ce5e5a045256b147cdd0f7391d211f4ce.

--- BankIntegration.API/Controllers/V1/ProductController.cs
+++ BankIntegration.API/Controllers/V1/ProductController.cs
@@ -30,6 +30,7 @@ public class ProductController : ControllerBase
     [Route("GetAll")]
     public async Task<IActionResult> GetAll()
     {
+        _logger.LogInformation("Api Called");
         var result = await _productService.GetAll();
         return result.Any() ? Ok(result) : NoContent();
     }
